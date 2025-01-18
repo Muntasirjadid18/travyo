cecdd4d4a6605f1050d9b3afff87135b600bdfd5
Ashiq Tasdid <ashiqtasdid5@gmail.com>
2025-01-18 22:04:30 +0600

    Fixed Bugs

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,42 +23,42 @@ const Navbar = () => {
         <div className={`navbar ${isOpen ? "active" : ""}`}>
           <ul className="navLists">
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                 Home
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                 Packages
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                 Shop
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                 About
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                Pages
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                 News
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink">
+              <a href="/" className="navLink">
                 Contact
               </a>
             </li>
             <li className="navItem">
-              <a href="#" className="navLink button">
+              <a href="/" className="navLink button">
                 Book Now
               </a>
             </li>
